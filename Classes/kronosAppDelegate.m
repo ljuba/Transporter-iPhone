c94b4125064207f5371f7948209ce835e2a28a32
Ljuba Miljkovic <ljuba.miljkovic@gmail.com>
2012-06-25 11:51:14 -0700

    New Data!

--- Classes/kronosAppDelegate.m
+++ Classes/kronosAppDelegate.m
@@ -474,53 +474,26 @@
         return persistentStoreCoordinator;
     }
 	
-	NSURL *storeUrl;
+	NSURL *storeURL;
 	
 	if (importing) {
 
-        storeUrl = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"kronos.sqlite"];        
+        storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"kronos.sqlite"];
         
 	}
 	else {
-		storeUrl = [NSURL fileURLWithPath: [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent: @"kronos.sqlite"]];
+		storeURL = [NSURL fileURLWithPath: [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent: @"kronos.sqlite"]];
         
 	}
 
-    
-
 	NSError *error = nil;
     persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-    if (![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeUrl options:[NSDictionary dictionaryWithObject:[NSNumber numberWithBool:1]
-                                                                                                                                                     forKey:NSReadOnlyPersistentStoreOption] error:&error])
-    {
-        /*
-         Replace this implementation with code to handle the error appropriately.
-         
-         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
-         
-         Typical reasons for an error here include:
-         * The persistent store is not accessible;
-         * The schema for the persistent store is incompatible with current managed object model.
-         Check the error message to determine what the actual problem was.
-         
-         
-         If the persistent store is not accessible, there is typically something wrong with the file path. Often, a file URL is pointing into the application's resources directory instead of a writeable directory.
-         
-         If you encounter schema incompatibility errors during development, you can reduce their frequency by:
-         * Simply deleting the existing store:
-         [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil]
-         
-         * Performing automatic lightweight migration by passing the following dictionary as the options parameter: 
-         [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES], NSMigratePersistentStoresAutomaticallyOption, [NSNumber numberWithBool:YES], NSInferMappingModelAutomaticallyOption, nil];
-         
-         Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
-         
-         */
+    if (![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+
         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
         abort();
     }    
-
-	
+    
     return persistentStoreCoordinator;
 }
 
